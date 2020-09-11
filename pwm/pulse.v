4cd52333a503024dcb6e4622d92f80202c433068
Matthias Bock <mail@matthiasbock.net>
2020-09-11 20:32:01 +0200

    Added buffering capability to pulse module

--- pwm/pulse.v
+++ pwm/pulse.v
@@ -6,19 +6,55 @@
 `ifndef PWM_PULSE_V
 `define PWM_PULSE_V
 
+`include "../buffer.v"
+
 module pulse
     #(
-        parameter bitwidth = 10
+        parameter bitwidth = 10,
+        parameter enable_double_buffering = 0
         )
     (
         input clock,
         input reset,
+        input load_enable,
         input [bitwidth-1:0] counter,
         input [bitwidth-1:0] tick_number_rising_edge,
         input [bitwidth-1:0] tick_number_falling_edge,
 
         output reg generated_signal
         );
+
+
+wire[bitwidth-1:0] internal_tick_number_rising_edge;
+wire[bitwidth-1:0] internal_tick_number_falling_edge;
+
+if (enable_double_buffering == 0)
+begin
+    assign internal_tick_number_rising_edge[bitwidth-1:0] = tick_number_rising_edge[bitwidth-1:0];
+    assign internal_tick_number_falling_edge[bitwidth-1:0] = tick_number_falling_edge[bitwidth-1:0];
+end
+else begin
+    buffer #(
+            .bitwidth       (bitwidth)
+        )
+        buffer_rising_edge(
+            .clock          (clock),
+            .load_enable    (load_enable),
+            .value_in       (tick_number_rising_edge[bitwidth-1:0]),
+            .value_out      (internal_tick_number_rising_edge[bitwidth-1:0])
+            );
+
+    buffer #(
+            .bitwidth       (bitwidth)
+        )
+        buffer_falling_edge(
+            .clock          (clock),
+            .load_enable    (load_enable),
+            .value_in       (tick_number_falling_edge[bitwidth-1:0]),
+            .value_out      (internal_tick_number_falling_edge[bitwidth-1:0])
+            );
+end
+
 
 initial generated_signal <= 0;
 
@@ -29,9 +65,9 @@ begin
         generated_signal <= 0;
     end
     else begin
-        if ((counter < tick_number_rising_edge) || (counter >= tick_number_falling_edge))
+        if ((counter < internal_tick_number_rising_edge) || (counter >= internal_tick_number_falling_edge))
             generated_signal <= 0;
-        else //if (counter >= tick_number_rising_edge)
+        else //if (counter >= internal_tick_number_rising_edge)
             generated_signal <= 1;
     end
 end
