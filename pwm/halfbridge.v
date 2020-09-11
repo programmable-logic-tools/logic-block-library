fd86b85f0a997fddcdcd180707faed0a00d22245
Matthias Bock <mail@matthiasbock.net>
2020-09-11 11:18:17 +0200

    Unsuccessful attempt to fix flat-top

--- pwm/halfbridge.v
+++ pwm/halfbridge.v
@@ -30,6 +30,12 @@ module pwm
         parameter tick_count_calculation_delay = 6,
 
         /**
+         * With this parameter enabled, both outputs will stop switching
+         * when one is set to an extreme value (0 respectively counter overflow-1).
+         */
+        parameter enable_flat_top = 1,
+
+        /**
          * When the turn-on duration of a switch exceeds it's maximum,
          * such that it is no longer possible to turn on the corresponding complementary switch,
          * this parameter configures, whether the PWM should switch to flat-top mode
@@ -83,77 +89,74 @@ module pwm
  * We need to remember, whether the last state was flat top on the lowside switch,
  * because in that case a deadtime must be inserted before turning on the highside switch.
  */
-localparam STATE_REGULAR_PWM = 0;
-localparam STATE_INTERMITTENT_HIGHSIDE = 1;
-localparam STATE_INTERMITTENT_LOWSIDE = 2;
-localparam STATE_FLAT_TOP_HIGHSIDE = 3;
-localparam STATE_FLAT_TOP_LOWSIDE = 4;
-localparam state_count = 5;
+localparam STATE_OFF = 0;
+localparam STATE_REGULAR_PWM = 1;
+localparam STATE_INTERMITTENT_HIGHSIDE = 2;
+localparam STATE_INTERMITTENT_LOWSIDE = 3;
+localparam STATE_FLAT_TOP_HIGHSIDE = 4;
+localparam STATE_FLAT_TOP_LOWSIDE = 5;
+localparam state_count = 6;
 
-reg[$clog2(state_count):0] state = STATE_REGULAR_PWM;
-reg[$clog2(state_count):0] previous_state = STATE_REGULAR_PWM;
+reg[$clog2(state_count):0] state = STATE_OFF;
+reg[$clog2(state_count):0] previous_state = STATE_OFF;
+reg[$clog2(state_count):0] suggested_state = STATE_OFF;
 
 
 /*
  * Calculate dutycycle values
  */
-reg[bitwidth:0] tick_number_rising_edge_highside  = 0;
-reg[bitwidth:0] tick_number_falling_edge_highside = 0;
-reg[bitwidth:0] tick_number_rising_edge_lowside   = 0;
-reg[bitwidth:0] tick_number_falling_edge_lowside  = 0;
+reg[bitwidth-1:0] tick_number_rising_edge_highside  = 0;
+reg[bitwidth-1:0] tick_number_falling_edge_highside = 0;
+reg[bitwidth-1:0] tick_number_rising_edge_lowside   = 0;
+reg[bitwidth-1:0] tick_number_falling_edge_lowside  = 0;
 
-reg[bitwidth:0] t1 = 0;
-reg[bitwidth:0] t2 = 0;
-reg[bitwidth:0] t3 = 0;
-reg[bitwidth:0] t4 = 0;
+reg[bitwidth-1:0] t1 = 0;
+reg[bitwidth-1:0] t2 = 0;
+reg[bitwidth-1:0] t3 = 0;
+reg[bitwidth-1:0] t4 = 0;
 
 initial calculation_error <= 0;
 
-/**
- * The maximum number of ticks a gate signal can be high,
- * before it doesn't make sense anymore
- * to turn on the respective complementary switch
- * (flat top mode or intermittent switching).
- */
-localparam threshold_complementary_switch_turnon = tick_count_period - deadtime_hs_to_ls - deadtime_ls_to_hs - minimum_driver_on_time;
+
+localparam threshold1 = 1;
+localparam threshold2 = minimum_driver_on_time;
+localparam threshold3 = tick_count_period - deadtime_hs_to_ls - deadtime_ls_to_hs - minimum_driver_on_time - 1;
+localparam threshold4 = tick_count_period - deadtime_hs_to_ls - deadtime_ls_to_hs - 2;
+
+wire highside_is_minimal = (tick_count_highside < minimum_driver_on_time);
+wire lowside_is_minimal  = (tick_count_lowside  < minimum_driver_on_time);
+wire both_on_times_minimal =
+        (
+            (highside_is_minimal != 0)
+         && (lowside_is_minimal  != 0)
+         );
+
+wire flat_bottom_highside, flat_bottom_lowside;
+wire flat_top_highside, flat_top_lowside;
+if (enable_flat_top == 0)
+begin
+    assign flat_bottom_highside = 0;
+    assign flat_top_highside = 0;
+    assign flat_bottom_lowside = 0;
+    assign flat_top_lowside = 0;
+end
+else begin
+    assign flat_bottom_highside = highside_is_minimal;
+    assign flat_bottom_lowside = lowside_is_minimal;
+    assign flat_top_highside = flat_bottom_lowside;
+    assign flat_top_lowside = flat_bottom_highside;
+end
+
 wire switch_highside_intermittently, switch_lowside_intermittently;
 if (enable_intermittent_switching == 0)
 begin
-    /*
-     * Switch to flat-top as soon as the complementary switch cannot be turned on anymore
-     */
-    localparam flat_top_threshold = threshold_complementary_switch_turnon;
     assign switch_highside_intermittently = 0;
     assign switch_lowside_intermittently = 0;
 end
 else begin
-    localparam flat_top_threshold = tick_count_period - 1;
-    assign switch_highside_intermittently =
-               (tick_count_highside >= threshold_complementary_switch_turnon)
-            && (tick_count_highside < flat_top_threshold);
-    assign switch_lowside_intermittently =
-               (tick_count_lowside >= threshold_complementary_switch_turnon)
-            && (tick_count_lowside < flat_top_threshold);
+    assign switch_highside_intermittently = lowside_is_minimal && (tick_count_highside < threshold4);
+    assign switch_lowside_intermittently = highside_is_minimal && (tick_count_lowside < threshold4);
 end
-
-wire highside_is_minimal = (tick_count_highside < minimum_driver_on_time);
-wire lowside_is_minimal  = (tick_count_lowside  < minimum_driver_on_time);
-
-wire flat_top_highside =
-            (tick_count_highside >= flat_top_threshold)
-        || ((highside_is_minimal == 0) && (lowside_is_minimal == 1))
-        ;
-
-wire flat_top_lowside =
-            (tick_count_lowside >= flat_top_threshold)
-        || ((highside_is_minimal == 1) && (lowside_is_minimal == 0))
-         ;
-
-wire both_on_times_minimal =
-        (
-            (highside_is_minimal == 1)
-         && (lowside_is_minimal  == 1)
-         );
 
 wire disable_pwm_immediately =
            (reset == 1)
@@ -169,44 +172,73 @@ always @(posedge clock)
 begin
     if (both_on_times_minimal)
     begin
+        suggested_state <= STATE_OFF;
         t1 <= 0;
         t2 <= 0;
         t3 <= 0;
         t4 <= 0;
     end
-    else if ((enable_intermittent_switching == 1) && (switch_highside_intermittently == 1))
+    else if (tick_count_highside < minimum_driver_on_time)
     begin
-        t1 <= 0;
-        t2 <= tick_count_highside;
-        t3 <= 0;
-        t4 <= 0;
-    end
-    else if ((enable_intermittent_switching == 1) && (switch_lowside_intermittently == 1))
-    begin
+        /*
+         * The high-side switch stays off.
+         */
         t1 <= 0;
         t2 <= 0;
-        t3 <= 0;
-        t4 <= tick_count_lowside;
+
+        if (tick_count_lowside >= tick_count_period-1)
+        begin
+            /*
+             * The low-side switch stays on (flat top mode).
+             */
+            suggested_state <= STATE_FLAT_TOP_LOWSIDE;
+            t3 <= 0;
+            t4 <= tick_count_period;
+        end
+        else begin
+            /*
+             * The low-side switch switches intermittently.
+             */
+             suggested_state <= STATE_INTERMITTENT_LOWSIDE;
+             t3 <= 0;
+             t4 <= tick_count_lowside;
+        end
     end
-    else if (flat_top_highside == 1)
+    else if (tick_count_lowside < minimum_driver_on_time)
     begin
-        t1 <= 0;
-        t2 <= tick_count_period;
+        /*
+         * The low-side switch stays off.
+         */
         t3 <= 0;
         t4 <= 0;
+
+        if (tick_count_highside >= tick_count_period-1)
+        begin
+            /*
+             * The high-side switch stays on (flat top mode).
+             */
+            suggested_state <= STATE_FLAT_TOP_HIGHSIDE;
+            t1 <= 0;
+            t2 <= tick_count_period;
+        end
+        else begin
+            /*
+             * The high-side switch switches intermittently.
+             */
+             suggested_state <= STATE_INTERMITTENT_HIGHSIDE;
+             t1 <= 0;
+             t2 <= tick_count_highside;
+        end
     end
-    else if (flat_top_lowside == 1)
-    begin
-        t1 <= 0;
-        t2 <= 0;
-        t3 <= 0;
-        t4 <= tick_count_period;
-    end
-    else if (previous_state == STATE_FLAT_TOP_LOWSIDE)
+    else if (
+        // (previous_state == STATE_INTERMITTENT_HIGHSIDE) ||
+        (previous_state == STATE_FLAT_TOP_LOWSIDE)
+        )
     begin
         /*
          * Regular PWM, but a deadtime must be added between lowside turn-off to highside turn-on
          */
+        suggested_state <= STATE_REGULAR_PWM;
         t1 = deadtime_ls_to_hs;
         t2 = t1 + tick_count_highside;
         t3 = t2 + deadtime_hs_to_ls;
@@ -214,6 +246,7 @@ begin
     end
     else begin
         // Regular PWM with deadtimes
+        suggested_state <= STATE_REGULAR_PWM;
         t1 = 0;
         t2 = tick_count_highside;
         t3 = t2 + deadtime_hs_to_ls;
@@ -250,8 +283,6 @@ counter #(
 /**
  * Update the internal PWM registers after the on- and off-time calculations are complete
  */
-reg disable_highside = 0;
-reg disable_lowside = 0;
 always @(posedge clock)
 begin
     if (calculation_complete == 1)
@@ -265,64 +296,7 @@ begin
         tick_number_falling_edge_lowside  <= t4;
 
         previous_state <= state;
-
-        if (switch_highside_intermittently == 1)
-        begin
-            state <= STATE_INTERMITTENT_HIGHSIDE;
-            disable_highside <= 0;
-            disable_lowside <= 1;
-        end
-        else if (switch_lowside_intermittently == 1)
-        begin
-            state <= STATE_INTERMITTENT_LOWSIDE;
-            disable_highside <= 1;
-            disable_lowside <= 0;
-        end
-        else if (flat_top_highside == 1)
-        begin
-            state <= STATE_FLAT_TOP_HIGHSIDE;
-            disable_highside <= 0;
-            disable_lowside <= 1;
-        end
-        else if (flat_top_lowside == 1)
-        begin
-            state <= STATE_FLAT_TOP_LOWSIDE;
-            disable_highside <= 1;
-            disable_lowside <= 0;
-        end
-        else begin
-            state <= STATE_REGULAR_PWM;
-            disable_highside <= 0;
-            disable_lowside <= 0;
-        end
-
-        /*
-         * Check, if the sum of all times - without the deadtimes because of the DCM mode -
-         * exceeds the period duration.
-         */
-        if (tick_count_highside + tick_count_lowside > tick_count_period)
-        begin
-            calculation_error <= 1;
-
-            // Disable highside
-            tick_number_rising_edge_highside  <= 0;
-            tick_number_falling_edge_highside <= 0;
-
-            // Disable lowside
-            tick_number_rising_edge_lowside   <= 0;
-            tick_number_falling_edge_lowside  <= 0;
-        end
-
-        if (calculation_error == 1)
-        begin
-            // Disable highside
-            tick_number_rising_edge_highside  <= 0;
-            tick_number_falling_edge_highside <= 0;
-
-            // Disable lowside
-            tick_number_rising_edge_lowside   <= 0;
-            tick_number_falling_edge_lowside  <= 0;
-        end
+        state <= suggested_state;
     end
 end
 
@@ -332,7 +306,7 @@ pulse #(
     )
     pulse_gate_highside (
         .clock                      (clock),
-        .reset                      (disable_pwm_immediately | disable_highside),
+        .reset                      (disable_pwm_immediately),
         .counter                    (tick_counter[bitwidth-1:0]),
         .tick_number_rising_edge    (tick_number_rising_edge_highside[bitwidth-1:0]),
         .tick_number_falling_edge   (tick_number_falling_edge_highside[bitwidth-1:0]),
@@ -343,21 +317,22 @@ pulse #(
  * Under no circumstances can highside and lowside be high at the same time.
  * This wire is a last line of protection.
  */
-wire lowside_output_unprotected;
+// wire lowside_output_unprotected;
 
 pulse #(
         .bitwidth                   (bitwidth)
     )
     pulse_gate_lowside (
         .clock                      (clock),
-        .reset                      (disable_pwm_immediately | disable_lowside),
+        .reset                      (disable_pwm_immediately),
         .counter                    (tick_counter[bitwidth-1:0]),
         .tick_number_rising_edge    (tick_number_rising_edge_lowside[bitwidth-1:0]),
         .tick_number_falling_edge   (tick_number_falling_edge_lowside[bitwidth-1:0]),
-        .generated_signal           (lowside_output_unprotected)
+        // .generated_signal           (lowside_output_unprotected)
+        .generated_signal           (lowside_output)
         );
 
-assign lowside_output = lowside_output_unprotected & (~highside_output);
+// assign lowside_output = lowside_output_unprotected & (~highside_output);
 
 /*
  * The last line of protection
