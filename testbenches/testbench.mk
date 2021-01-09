709d2e6b7a05b3d12164c5760c06940ec4246b36
Matthias Bock <mail@matthiasbock.net>
2021-01-09 20:19:11 +0100

    Enhanced testbench.mk

--- testbenches/testbench.mk
+++ testbenches/testbench.mk
@@ -4,6 +4,15 @@
 # common to all testbenches
 #
 
+ifndef LIB_ROOT
+LIB_ROOT = ..
+endif
+
+IVERILOG = iverilog
+VVP = vvp
+GTKWAVE = gtkwave
+GTKW_MANGLER = $(LIB_ROOT)/tools/gtkw_mangler.py
+
 TESTBENCHES = $(SRCS:.v=.vvp)
 WAVEFILES = $(TESTBENCHES:.vvp=.lx2)
 WAVEPROJECTS = $(WAVEFILES:.lx2=.gtkw)
@@ -11,23 +20,32 @@ WAVEPROJECTS = $(WAVEFILES:.lx2=.gtkw)
 
 simulate: $(WAVEFILES)
 
+.PRECIOUS: %.gtkw %.vvp %.lx2 %.vcd
+
 %.vvp: %.v
-	iverilog $(IVFLAGS) -s test -o $@ $<
+	$(IVERILOG) $(IVFLAGS) -s test -o $@ $<
 
 %.lx2: %.vvp
-	vvp $^ -lxt2
+	$(VVP) $^ -lxt2
 	mv dump.lx2 $(^:.vvp=.lx2)
 
 %.vcd: %.vvp
-	vvp $^ -vcd
+	$(VVP) $^ -vcd
 	mv dump.vcd $(^:.vvp=.vcd)
 
 # Before the results can be shown, they must be generated.
+.PHONY: show
 show: simulate $(WAVEPROJECTS)
 
+.PHONY: %.gtkw
 %.gtkw: %.lx2
-	gtkwave $@ &
+	touch $@
+	$(GTKWAVE) $@
+	$(GTKW_MANGLER) $(WAVEPROJECTS)
+
+.PHONY: gtkw_mangler
+gtkw_mangler:
+	$(GTKW_MANGLER) $(WAVEPROJECTS)
 
 clean-testbench:
 	rm -f $(WAVEFILES) $(TESTBENCHES)
-
